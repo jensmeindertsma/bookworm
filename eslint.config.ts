92849e4fc84db9b15b23946151a27725eb626192
Jens Meindertsma <mail@jensmeindertsma.com>
2025-07-07 13:16:40 +0200

    Ignore generated client

--- eslint.config.ts
+++ eslint.config.ts
@@ -7,7 +7,7 @@ export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    ignores: ["build", ".react-router"],
+    ignores: ["build", "src/generated", ".react-router"],
   },
   {
     plugins: {
