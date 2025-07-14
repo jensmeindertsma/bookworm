552e2a5acdf824501c421ba296faf3e72a0115ab
Jens Meindertsma <mail@jensmeindertsma.com>
2025-07-14 06:44:46 +0200

    log error

--- src/layouts/error.tsx
+++ src/layouts/error.tsx
@@ -6,6 +6,8 @@ export default function ErrorLayout() {
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
+  console.error(error);
+
   if (isRouteErrorResponse(error)) {
     return (
       <>
